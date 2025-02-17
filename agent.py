0266c39880194e56e706631f3e1163a1560c6af4
Md Mahfuzur Rahman <mahfuzur@dana.money>
2025-02-17 19:51:53 +0600

    fix logo name

--- agent.py
+++ agent.py
@@ -237,7 +237,8 @@ class Agent:
 
         # non-config vars
         self.number = number
-        self.agent_name = f"Agent {self.number}"
+        self.agent_name = f"Kube Agent"
+        # self.agent_name = f"Agent {self.number}"
 
         self.history = history.History(self)
         self.last_user_message: history.Message | None = None
